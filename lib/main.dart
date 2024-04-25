e4a9db2b6b93df27ee6b83cb0a16e2180b229e65
frc-wolverines <rosemont5274@gmail.com>
2024-04-24 21:50:48 -0700

    removed clutter for restart

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,3 @@
-import 'package:Atlas/pages/home.dart';
-import 'package:flutter/material.dart';
-
 void main() {
-  runApp(const Atlas());
-}
 
-class Atlas extends StatefulWidget {
-  const Atlas({super.key});
-
-  @override
-  State<Atlas> createState() => AtlasState();
-}
-
-class AtlasState extends State<Atlas> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Atlas',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        colorSchemeSeed: const Color.fromARGB(255, 255, 255, 255),
-        useMaterial3: true
-      ),
-      home: const HomePage()
-    );
-  }
 }
